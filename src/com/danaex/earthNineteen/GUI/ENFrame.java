3924b167656d8c4db143e69b1fa789b17730d9dc
DanaeX <antoine.andurao@yahoo.fr>
2017-10-22 18:19:41 +0200

    First commit from Eclipse. GitHub + Eclipse are working now.

--- src/com/danaex/earthNineteen/GUI/ENFrame.java
+++ src/com/danaex/earthNineteen/GUI/ENFrame.java
@@ -1,13 +1,17 @@
 package com.danaex.earthNineteen.GUI;
 
-import com.danaex.earthNineteen.core.FileFinder;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.security.Key;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import com.danaex.earthNineteen.core.FileFinder;
 
 public class ENFrame extends JFrame {
 
